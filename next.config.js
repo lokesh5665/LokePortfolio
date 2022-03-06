ca40e46c4ee001b4c9d4c1751a63b4ffe6488121
lokesh5665 <30732403+lokesh5665@users.noreply.github.com>
2022-03-07 00:46:02 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
+const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
-
+    assetPrefix: isProd ? '/LokePortfolio/' : '',
     reactStrictMode: false,
     images: {
         domains: ["avatars.githubusercontent.com"],
